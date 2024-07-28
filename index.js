a42f0a62e76b88f99678360ab127a86c4df44079
t-aswath <119417646+t-aswath@users.noreply.github.com>
2024-07-28 08:27:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Define a route for the root URL
 app.get("/", (req, res) => {
-  res.send("Hellosfssfdsf, W");
+  res.send("Hi FossHack this is :bitspace, W");
 });
 
 // Start the server on port 3000
